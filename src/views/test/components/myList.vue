ae9f455c11e311c6f40493da86ce27f423e22fd8
STARBOOK\84634 <lo@163.COM>
2024-07-24 21:34:31 +0800

    add待办列表 修改、去重、增加页码

--- src/views/test/components/myList.vue
+++ src/views/test/components/myList.vue
@@ -1,53 +1,80 @@
 <template>
     <div class="my-list-wrap">
         <!-- 输入框 -->
-        <el-input v-model="addText" style="max-width: 600px; height: 36px" placeholder="请输入您的待办事项"
-            class="input-with-select">
-            <template #append>
-                <el-button type="primary" @click="handleListAdd">新增</el-button>
-            </template>
-        </el-input>
-        <el-button @click="handleBatchDel">批量删除</el-button>
+        <div class="operate-header">
+            <el-input v-model.trim="addText" style="max-width: 600px; height: 36px" placeholder="请输入您的待办事项"
+                class="input-with-select">
+                <template #append>
+                    <el-button type="primary" @click="handleListAdd">新增</el-button>
+                </template>
+            </el-input>
+            <el-button @click="handleBatchDel" style=" height: 36px">批量删除</el-button>
+        </div>
+
         <!-- 列表 -->
         <P class="list-title">今日待办事项清单</P>
-        <el-table :border="true" :data="tableData" style="width: 100%" @select="handleSelectionChange"
-            @select-all="handleSelectionChange">
+        <el-table :border="true" :data="tableData.slice((pageNum - 1) * pageSize, pageNum * pageSize)"
+            style="width: 100%" @select="handleSelectionChange" @select-all="handleSelectionChange">
             <el-table-column type="selection" width="80px" />
-            <el-table-column prop="title" label="标题" />
+            <el-table-column prop="title" label="标题" min-width="180px">
+                <template #default="scope">
+                    <el-input type="text" v-if="!!scope.row.isEdit" v-model.trim="questionInput"></el-input>
+                    <p v-else>{{ scope.row.title }}</p>
+                </template>
+
+            </el-table-column>
+
+            <el-table-column label="操作" min-width="80">
+                <template #default="scope">
+                    <span v-if="scope.row.isEdit">
+                        <el-button type="primary" size="small" @click="confirmEdit(scope.row)">
+                            确定
+                        </el-button>
+                        <el-button type="danger" size="small" @click="cancelEdit(scope.row)">
+                            取消
+                        </el-button>
+                    </span>
+                    <span v-else>
+                        <el-button link type="primary" size="small" @click="handleClick(scope.row)">
+                            编辑
+                        </el-button>
+                        <el-popconfirm title="确认删除这条信息吗?" confirm-button-text="确认" cancel-button-text="取消"
+                            @confirm="handleDeleteAction(scope.row, 1)">
+                            <template #reference>
+                                <el-button link type="danger" size="small">删除</el-button>
+                            </template>
+                        </el-popconfirm>
+                    </span>
+
+                </template>
+
+            </el-table-column>
             <el-table-column prop="content" label="内容" />
             <el-table-column prop="tags" label="标签" />
-            <el-table-column prop="date" label="创建日期" />
             <el-table-column prop="username" label="创建人" />
-            <el-table-column fixed="right" label="Operations" min-width="120">
-                <template #default="scope">
-                    <el-button link type="primary" size="small" @click="handleClick">
-                        编辑
-                    </el-button>
-                    <el-popconfirm title="确认删除这条信息吗?" confirm-button-text="确认" cancel-button-text="取消"
-                        @confirm="handleDeleteAction(scope.row, 1)">
-                        <template #reference>
-                            <el-button link type="danger" size="small">删除</el-button>
-                        </template>
-                    </el-popconfirm>
-                </template>
-            </el-table-column>
+            <el-table-column prop="date" label="创建日期" />
+
             <template #empty>
                 <el-empty description="暂无数据" :image-size="80"></el-empty>
             </template>
         </el-table>
-    </div>
 
+        <!-- 页码 -->
+        <el-pagination v-if="total > 0" v-model:current-page="pageNum" v-model:page-size="pageSize"
+            layout="total, prev, pager, next" :total="total" class="common-pagination-line" />
+    </div>
 </template>
 
 <script lang="ts" setup>
-import { ref } from 'vue'
-import { ElMessage, descriptionProps } from 'element-plus'
+import { computed, ref } from 'vue'
+import { ElMessage, } from 'element-plus'
 import dayjs from 'dayjs';
 const addText = ref('')
 
-const handleClick = () => {
-    console.log('click')
-}
+// 分页相关
+const pageNum = ref(1)
+const pageSize = ref(6)
+const total = computed(() => tableData.value.length)
 
 const tableData = ref([
     {
@@ -58,10 +85,52 @@ const tableData = ref([
         username: 'RITA'
     }
 ])
+// 修改
+const questionInput = ref('')
+const cacheTitle = ref('')
+const handleClick = (row) => {
+    tableData.value.map(grid => {
+        return grid['isEdit'] = false
+    })
+    questionInput.value = row.title
+    cacheTitle.value = row.title
+    row['isEdit'] = true
+}
+// 确定修改
+const confirmEdit = (row) => {
+    if (!questionInput.value) return
+    // 校验是否重复
+    if (validateDateRepeat(questionInput.value)) {
+        ElMessage({
+            message: '您输入的数据有重复，请重新输入。',
+            type: 'warning',
+        })
+        return
+    }
+    ElMessage({
+        message: '编辑成功',
+        type: 'success',
+    })
+    row['title'] = questionInput.value
+    row['isEdit'] = false
+}
+// 取消修改
+const cancelEdit = (row) => {
+    row['title'] = questionInput.value
+    row['isEdit'] = false
+}
+
 // 新增
 const handleListAdd = () => {
     if (!addText.value) return
     // 校验是否重复
+    if (validateDateRepeat(addText.value)) {
+        ElMessage({
+            message: '您输入的数据有重复，请重新输入。',
+            type: 'warning',
+        })
+        return
+    }
     tableData.value.unshift({
         title: addText.value,
         content: '自定义标题内容',
@@ -69,6 +138,16 @@ const handleListAdd = () => {
         date: dayjs(new Date()).format('YYYY-MM-DD HH:mm:ss'),
         username: 'RITA'
     })
+}
+// 校验是否重复
+const validateDateRepeat = (title) => {
+    let flag = false
+    tableData.value.map((item) => {
+        if (item.title === title) {
+            flag = true
+        }
+    })
+    return flag
 }
 // 删除
 const userSelectList = ref([])
@@ -108,6 +187,17 @@ const delAction = (ids) => {
     .list-title {
         font-weight: 600;
         margin: 40px 0 20px;
+    }
+
+    .operate-header {
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+    }
+
+    .common-pagination-line {
+        justify-content: flex-end;
+        margin-top: 20px;
     }
 }
 </style>
