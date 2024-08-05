d374d6f844bd42d2685fdf7df4bf36daf8832cc2
STARBOOK\84634 <lo@163.COM>
2024-08-05 18:47:40 +0800

    add 自定义的按钮权限判断指令

--- src/directives/index.js
+++ src/directives/index.js
@@ -1,5 +1,6 @@
 // 使用了vueuse获取元素是否出现在视口中的函数方法
 import { useIntersectionObserver } from '@vueuse/core'
+import { onBeforeMount, onBeforeUpdate } from 'vue'
 
 // 创建 图片懒加载的自定义指令
 export const lazyImgPlugin = {
@@ -16,6 +17,43 @@ export const lazyImgPlugin = {
             stop()
           }
         })
+      }
+    })
+  }
+}
+
+// 后台返回的权限code列表存入store
+const hasBtnPermission = (value)=>{
+  // 判断是否是数组 是就递归继续判断具体code
+  if(Array.isArray(value)){
+    return value.some((code=> hasBtnPermission(code)))
+  }else{
+    // mock code list
+    return ['savePublic','saveMine','downloadPublic'].indexOf(value) !== -1
+  }
+}
+
+// 自定义的按钮权限判断指令
+export const hasPermission = {
+  install(app) {
+    app.directive('permission', {
+      // el 指令绑定的元素
+      // binding 指令等号后面的内容 binding.value这里是传入的权限code
+      beforeMount(el,binding){
+        const {value, instance} = binding
+        instance._originDisplayStyle = el.style.display
+        if(!hasBtnPermission(value)){
+          el.style.display = 'none'
+        }
+      },
+      beforeUpdate(el,binding){
+        const {value, instance} = binding
+        instance._originDisplayStyle = el.style.display
+        if(!hasBtnPermission(value)){
+          el.style.display = 'none'
+        }else{
+          el.style.display = instance._originDisplayStyle
+        }
       }
     })
   }
